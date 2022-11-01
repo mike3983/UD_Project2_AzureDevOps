d7e8f7aa87108db4d6a1e81c967691a4da1ad860
mike3983 <mgalla4@gmail.com>
2022-11-01 16:23:22 -0500

    update html

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home - let's go to Las Vegas with our prediction</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
